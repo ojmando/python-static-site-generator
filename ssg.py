e68292a34530ca89e164f7c16d00d3f23b2aa74e
Ognjan Janev <ognjan.oj.janev@bmw.de>
2022-03-23 16:47:13 +0100

    Fix last error in ssg

--- ssg.py
+++ ssg.py
@@ -8,8 +8,7 @@ def main(source="content", dest="dist"):
         "source": source,
         "dest": dest
     }
-    my_site = Site(**config)
-    my_site.build()
+    Site(**config).build()
 
 
 typer.run(main)

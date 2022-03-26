67918fca78eb32370fed08db928ebba1e4861ca0
Ognjan Janev <ognjan.oj.janev@bmw.de>
2022-03-26 11:42:32 +0100

    Add new parsers

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,6 +1,13 @@
 import shutil
+import sys
+from distutils import core
 from pathlib import Path
 from typing import List
+
+from docutils.core import publish_parts
+from markdown import markdown
+
+from ssg.content import Content
 
 
 class Parser:
@@ -30,3 +37,25 @@ class ResourceParser(Parser):
 
     def parse(self, path: Path, source: Path, dest: Path):
         self.copy(path, source, dest)
+
+
+class MarkdownParser(Parser):
+    extensions = [".md", ".markdown"]
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        content = Content.load(self.read(path))
+        html = markdown(content.body)
+        self.write(path, dest, html)
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(
+            path.name, content))
+
+
+class ReStructuredText(Parser):
+    extensions = [".rst"]
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        content = Content.load(self.read(path))
+        html = publish_parts(content.body, writer_name="html5")
+        self.write(path, dest, html["html_body"])
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(
+            path.name, content))
